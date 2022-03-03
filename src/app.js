e529e41d1ef18fe85b2ca2aa7198d796769af88f
Elikdev <koladeodutoye9913@gmail.com>
2022-03-03 19:49:25 +0100

    testing

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.post(
   "/split-payments/compute",
-  // computeFeeValidation(),
+  computeFeeValidation(),
   async (req, res) => {
     const { ID, Amount, SplitInfo } = req.body
 
